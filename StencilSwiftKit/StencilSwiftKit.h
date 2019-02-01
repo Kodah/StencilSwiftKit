8ec56ac620d8942f0cd212d777376fef277be998
Tom Sugarev <thomas.sugarev@sky.uk>
2019-02-01 01:18:52 +0000

    Add target

---
+++ StencilSwiftKit/StencilSwiftKit.h
@@ -0,0 +1,19 @@
+//
+//  StencilSwiftKit.h
+//  StencilSwiftKit
+//
+//  Created by Sugarev, Thomas (iOS Developer) on 01/02/2019.
+//  Copyright © 2019 AliSoftware. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for StencilSwiftKit.
+FOUNDATION_EXPORT double StencilSwiftKitVersionNumber;
+
+//! Project version string for StencilSwiftKit.
+FOUNDATION_EXPORT const unsigned char StencilSwiftKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StencilSwiftKit/PublicHeader.h>
+
+
